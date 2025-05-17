b19a420ea111b3db6ce20edb5bff41f87abd3479
meermansoor <meermansoor002@gmail.com>
2025-05-18 03:01:36 +0500

    idk what i am doing but im doing something

--- App.js
+++ App.js
@@ -1,55 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import NextUpScreen from './src/screens/NextUpScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
-import WelcomeNextUpScreen from './src/screens/WelcomeNextUpScreen';
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native"; // ✅ Added Text here
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen 
-          name="Welcome" 
-          component={WelcomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="WelcomeNextUp" 
-          component={WelcomeNextUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="NextUp" 
-          component={NextUpScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <View style={styles.hello}>
+      <Text style={{ color: 'white' }}>Hello App</Text>
+    
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  hello: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
-
-
